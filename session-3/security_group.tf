823a3005b61b10996039b5b66c41b155819ee64b
EC2 Default User <ec2-user@ip-172-31-33-174.us-east-2.compute.internal>
2022-10-15 22:37:06 +0000

    dev.tfvars

---
+++ session-3/security_group.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "first_sg" {
+  name        = "web_sg"
+  description = "This is sg for web instance"
+  #vpc_id is optional better to have if want to use specific vpc
+}
+
+resource "aws_security_group_rule" "web_ingress" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.first_sg.id
+}
+
+resource "aws_security_group_rule" "web_egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1" 
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.first_sg.id
+}
