9cba4c4f8e072d5891d583221c75783e7e09cb27
EC2 Default User <ec2-user@ip-172-31-33-174.us-east-2.compute.internal>
2022-10-18 06:02:16 +0000

    homework

--- homework/route_table.tf
+++ homework/route_table.tf
@@ -10,3 +10,28 @@ resource "aws_route_table_association" "public_subnet_1"{
     route_table_id = aws_route_table.public_rt.id
 
 }
+
+resource "aws_route_table_association" "public_subnet_2"{
+    subnet_id = aws_subnet.public_subnet_2.id
+    route_table_id = aws_route_table.public_rt.id
+
+}
+
+resource "aws_route_table" "private_rt"{
+    vpc_id = aws_vpc.first_vpc.id
+    tags = {
+        Name = var.private_route_table
+    }
+}
+
+resource "aws_route_table_association" "private_subnet_1"{
+    subnet_id = aws_subnet.private_subnet_1.id
+    route_table_id = aws_route_table.private_rt.id
+
+}
+
+resource "aws_route_table_association" "private_subnet_2"{
+    subnet_id = aws_subnet.private_subnet_2.id
+    route_table_id = aws_route_table.private_rt.id
+
+}
