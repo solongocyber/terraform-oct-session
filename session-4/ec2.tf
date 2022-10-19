9e750055d89fb5a3e44e698fd17e57a1cfdf3841
EC2 Default User <ec2-user@ip-172-31-33-174.us-east-2.compute.internal>
2022-10-19 01:11:49 +0000

    seession-4

--- session-4/ec2.tf
+++ session-4/ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "main" {
   ami           = data.aws_ami.amazon_linux2.image_id
   instance_type = var.instance_type
   vpc_security_group_ids = [ aws_security_group.first_sg.id ]
+  user_data = file("user_data.sh")
   tags = {
     Name        = var.env # instance-name.  variable=env ,default=value (dev)
     
--- session-4/sg.tf
+++ session-4/sg.tf
@@ -6,8 +6,8 @@ resource "aws_security_group" "first_sg" {
 
 resource "aws_security_group_rule" "web_ingress" {
   type              = "ingress"
-  from_port         = 22
-  to_port           = 22
+  from_port         = 80
+  to_port           = 80
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.first_sg.id
