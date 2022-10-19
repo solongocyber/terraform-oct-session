e5d5d6b4aff6092b3b77557747ddad3829eb8cdf
EC2 Default User <ec2-user@ip-172-31-33-174.us-east-2.compute.internal>
2022-10-19 00:16:46 +0000

    session-4

---
+++ session-4/variable.tf
@@ -0,0 +1,10 @@
+variable "env" {
+    type = string
+    description = "This is a variable for Environment"
+    default = "dev"
+}
+
+variable "instance_type" {
+type = string
+default = "t2.micro"
+}
